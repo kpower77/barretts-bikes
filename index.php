d1b7495ea303847fa763b0d4d658d6b14c22005c
kpower77 <kpower77@gmail.com>
2016-09-22 20:29:24 +0000

    Updated Index Page

--- index.php
+++ index.php
@@ -33,6 +33,62 @@
     <?php
         echo 'Hello world from Cloud9!';
     ?>
+    
+    <!-- Bootstrap Wrapper -->
+    <div class="container">
+      
+      <!-- Page Header -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Barrett's Bikes</h1>  
+        </div>
+      </div>
+      <!-- End of Page Header -->
+      
+      <!-- Navbar -->
+      <div class="container">
+        <div>
+          <nav class="navbar navbar-inverse">
+            <ul class="nav navbar-nav">
+              <li class="active">Home</li>
+              <li class="active">Bikes</li>
+              <li class="active">About</li>
+              <li class="active">Location</li>
+            </ul> 
+          </nav>
+        </div>
+      </div>
+      <!-- End of Navbar -->
+      
+      <!-- Comments Form -->
+      <div class="container">
+        <div>
+          <form role="form" method="post" id="form" ation="comment.xml">
+            <h2>Contact Us</h2>
+            
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control" id="name" name="Name">
+            </div>
+            
+            <div class="form-group">
+              <label for="name">Email</label>
+              <input type="text" class="form-control" id="email" name="Email">
+            </div>
+            
+            <div class="form-group">
+              <label for="name">Comment</label>
+              <input type="text" class="form-control" id="comment" name="Comment">
+            </div>
+          </form>
+        </div>
+      </div>
+      <!-- End of Comments Form -->
+      
+      
+    </div>
+    
+    
 
     <!-- Bootstrap minified CSS Link -->
     <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
