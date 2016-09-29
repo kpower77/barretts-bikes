4db6f7229d8f1f50dee8c4d3cde1bab9a0a421b5
kpower77 <kpower77@gmail.com>
2016-09-29 18:39:28 +0000

    Added Contact Page

--- contact.php
+++ contact.php
@@ -34,20 +34,6 @@
     <!-- Bootstrap Wrapper -->
     <div class="container">
       
-    <!-- Jumbotron --> 
-     <div class="container">
-      <div class="jumbotron">
-        <h1>Barret's Bikes</h1>      
-        <p>Welcome to Barret's Bikes.</p>
-      </div>
-        <p>Enter some text here</p>      
-    </div>
-    <!-- End of Jumbotron -->
-     
-     
-     
-     
-     
       <!-- Page Header -->
       <div class="container">
         <div class="page-header">
@@ -61,9 +47,9 @@
         <div>
           <nav class="navbar navbar-inverse">
             <ul class="nav navbar-nav">
-              <li class="active">Home</li>
+              <li class="active"><a href="index.php">Home</a></li>
               <li class="active">Bikes</li>
-              <li class="active"><a href="contact.php">Contact Us</a></li>
+              <li class="active">Contact Us</li>
               <li class="active">Location</li>
             </ul> 
           </nav>
@@ -71,7 +57,30 @@
       </div>
       <!-- End of Navbar -->
       
-     
+      <!-- Comments Form -->
+      <div class="container">
+        <div>
+          <form role="form" method="post" id="form" ation="comment.xml">
+            <h2>Contact Us</h2>
+            
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control" id="name" name="Name">
+            </div>
+            
+            <div class="form-group">
+              <label for="name">Email</label>
+              <input type="text" class="form-control" id="email" name="Email">
+            </div>
+            
+            <div class="form-group">
+              <label for="name">Comment</label>
+              <input type="text" class="form-control" id="comment" name="Comment">
+            </div>
+          </form>
+        </div>
+      </div>
+      <!-- End of Comments Form -->
       
       
     </div>
